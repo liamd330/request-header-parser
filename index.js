0b7ebcdb510b7db2948484b563d6566895251a6f
Liam Donovan <liamdonovan245@gmail.com>
2017-05-04 18:02:57 -0400

    ip

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8000;
 
 app.get('/', function (request, response) {
     
-    var ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+    var ip = req.connection.remoteAddress;
     var language = JSON.stringify(request.headers["accept-language"]);
     var os = JSON.stringify(request.headers["user-agent"]);
     
